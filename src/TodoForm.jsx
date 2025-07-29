5a33ab37e802c3b6420a96dc4fc4d177da031f4f
pleaseHelpImDumb <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-07-29 17:06:40 -0400

    Todo functionality

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -1,10 +1,27 @@
 import "./TodoForm.css";
+import { useRef } from "react";
 
-function TodoForm() {
+function TodoForm({ onAddTodo }) {
+  const todoTitleInput = useRef("");
+
+  function handleAddTodo(event) {
+    event.preventDefault();
+    const title = event.target.title.value;
+    onAddTodo(title);
+    event.target.title.value = "";
+    todoTitleInput.current.focus();
+  }
+
   return (
-    <form action="">
+    <form onSubmit={handleAddTodo}>
       <label htmlFor="todoTitle">Todo</label>
-      <input id="todoTitle" type="text" placeholder="Learn React..." />
+      <input
+        ref={todoTitleInput}
+        name="title"
+        id="todoTitle"
+        type="text"
+        placeholder="Learn React..."
+      />
       <button>Add Todo</button>
     </form>
   );
--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,17 +1,11 @@
 import "./TodoList.css";
 import TodoListItem from "./TodoListItem.jsx";
 
-function TodoList() {
-  const todos = [
-    { id: 1, title: "clean kitchen" },
-    { id: 2, title: "take notes" },
-    { id: 3, title: "feed cat" },
-  ];
-
+function TodoList({ todoList }) {
   return (
     <ul>
-      {todos.map((todo) => (
-        <TodoListItem key={todo.id} title={todo.title}></TodoListItem>
+      {todoList.map((todo) => (
+        <TodoListItem key={todo.id} todo={todo}></TodoListItem>
       ))}
     </ul>
   );
