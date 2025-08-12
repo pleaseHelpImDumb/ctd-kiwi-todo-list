d79c74c64457633055a0dde47a3070fca145ec34
pleaseHelpImDumb <31854027+pleaseHelpImDumb@users.noreply.github.com>
2025-08-12 17:08:34 -0400

    features/shared and moved files and changed imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
-import TodoList from "./TodoList.jsx";
-import TodoForm from "./TodoForm.jsx";
+import TodoList from "./features/TodoList/TodoList.jsx";
+import TodoForm from "./features/TodoList/TodoForm.jsx";
 import { useState } from "react";
 
 function App() {
